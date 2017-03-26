426254e4e3f809da5496d60aed69d57479c124a3
xyp <xyp@kali.com>
2017-03-26 21:24:10 +0800

    add elementui in vue cli project

--- xiyuanpeng_front/src/App.vue
+++ xiyuanpeng_front/src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app">
-    <img src="assets/logo.png">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <el-button @click.native="buttonClick">Let's go</el-button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data () {
+    return {
+      msg: 'xieyuanpeng.in index'
+    }
+  },
+
+  methods: {
+    buttonClick () {
+      this.$notify({
+        title: 'Just a notify test',
+        message: 'This is just a test notify.',
+        duration: 6000
+      })
+    }
+  }
 }
 </script>
 
