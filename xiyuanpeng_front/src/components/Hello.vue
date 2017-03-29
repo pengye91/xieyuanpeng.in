a1d9618864fd72de641f646a8d9bc0f9a8945108
xyp <xyp@kali.com>
2017-03-29 18:59:56 +0800

    add elementui in vue cli project

--- xiyuanpeng_front/src/components/Hello.vue
+++ xiyuanpeng_front/src/components/Hello.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <el-col :span="8">
+      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+        <el-submenu index="1">
+          <template slot="title"><i class="el-icon-message"></i>导航一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
+        <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+      </el-menu>
+    </el-col>
+    <h1>{{ name }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
@@ -11,6 +27,7 @@
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     <h2>Ecosystem</h2>
+    <a href="http://www.baidu.com"><el-button><i class="el-icon-arrow-right"></i></el-button></a>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -22,10 +39,18 @@
 
 <script>
 export default {
-  name: 'hello',
   data () {
     return {
+      name: 'hello',
       msg: 'Hello'
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
