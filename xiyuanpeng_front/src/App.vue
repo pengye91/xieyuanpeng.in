31efe5ad6c4b152b455ef88fbe3647c433e4a07c
xyp <xyp@kali.com>
2017-04-02 21:18:02 +0800

    modified menu part, starting building click events

--- xiyuanpeng_front/src/App.vue
+++ xiyuanpeng_front/src/App.vue
@@ -4,10 +4,48 @@
     background: #f5f7f9;
   }
 
+  .ivu-menu-item {
+    font-size: 12px;
+    margin-left: 6px;
+  }
+
+  .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item {
+    font-size: 15px;
+    padding-left: 45px;
+    border-bottom: 0px;
+  }
+
+  .ivu-input-wrapper {
+    width: 15%;
+    float: right;
+    top: auto;
+    right: 3%;
+  }
+
+  /*.ivu-input-wrapper-large .ivu-input-icon {*/
+    /*line-height: 60px;*/
+  /*}*/
+
+  .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item:hover {
+    border-bottom: 0px;
+    height: 59px;
+    background: #f6f6f6 none repeat scroll 0 0;
+  }
+
+  .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item-selected {
+    background: #e6e6e6;
+  }
+
+  .ivu-menu-light.ivu-menu-horizontal .ivu-menu-item-active {
+    background: #e6e6e6 !important;
+  }
+  /*vertical*/
+  .ivu-menu-light.ivu-menu-vertical .ivu-menu-item-active:not(.ivu-menu-submenu) {
+    background: #cbcbcb;
+    border-right: 0px;
+  }
+
   .layout-logo {
-    /*width: 100px;*/
-    /*height: 30px;*/
-    /*background: #5b6270;*/
     border-radius: 3px;
     float: left;
     position: relative;
@@ -17,17 +55,16 @@
 
   .layout-search {
     width: 15%;
-    /*background: #5b6270;*/
-    /*border-radius: 10px;*/
     float: right;
-    position: relative;
     top: auto;
     right: 3%;
   }
 
-  .layout-nav {
-    width: 420px;
-    margin: 0 auto;
+  .layout-nav li {
+    left: 25%;
+    font-size: 15px;
+    float: left;
+    padding: 0 40px;
   }
 
   .layout-assistant {
@@ -36,13 +73,17 @@
     height: inherit;
   }
 
+  .layout-left {
+    left: 0;
+  }
+
   .layout-breadcrumb {
     padding: 10px 15px 0;
   }
 
   .layout-content {
     min-height: 200px;
-    margin: 15px;
+    margin: 10px;
     overflow: hidden;
     background: #fff;
     border-radius: 4px;
@@ -60,7 +101,7 @@
 </style>
 <template>
   <div class="layout">
-    <Menu mode="horizontal" theme="light" active-name="1">
+    <Menu mode="horizontal" theme="light">
       <div class="layout-logo">
         <img src="./assets/logo.png" alt="logo" height="40" width="40">
       </div>
@@ -71,22 +112,23 @@
         <Menu-item name="2">
           摄影作品
         </Menu-item>
-        <Menu-item name="4">
-          联系我吧
+        <Menu-item name="3">
+          联系我
         </Menu-item>
       </div>
-      <Input size="large" class="layout-search" icon="search"
+      <Input size="large" icon="search"
              placeholder="请输入搜索内容..." v-model="search"></Input>
-        <!--<Button slot="append" icon="search"></Button>-->
     </Menu>
     <div class="layout-content">
       <Row>
         <i-col span="5">
-          <Menu active-name="1-2" :open-names="['1']">
+          <Menu active-name="2-1" :open-names="['2']"
+                class="layout-left" width="70%">
             <Submenu name="1">
               <template slot="title">
-                <Icon type="document-text"></Icon>
+                <Icon type="ios-book" size="16"></Icon>
                 技术博客
+
               </template>
               <Menu-item name="1-1">Python</Menu-item>
               <Menu-item name="1-2">Django</Menu-item>
@@ -95,19 +137,23 @@
             </Submenu>
             <Submenu name="2">
               <template slot="title">
-                <Icon type="ios-keypad"></Icon>
+                <Icon type="images" size="15"></Icon>
                 摄影作品
+
               </template>
               <Menu-item name="2-1">项目 1</Menu-item>
               <Menu-item name="2-2">项目 2</Menu-item>
             </Submenu>
             <Submenu name="3">
               <template slot="title">
-                <Icon type="ios-analytics"></Icon>
+                <Icon type="android-contact" size="16"></Icon>
                 联系我吧
+
               </template>
               <Menu-item name="3-1">
-                <Icon type="social-github" size="18"></Icon>
+                <Icon type="social-github" size="14"></Icon>
+                github
+
               </Menu-item>
               <Menu-item name="3-2">wechat</Menu-item>
             </Submenu>
@@ -120,6 +166,7 @@
     </div>
     <div class="layout-copy">
       &copy; XieYuanpeng.in
+
     </div>
   </div>
 </template>
