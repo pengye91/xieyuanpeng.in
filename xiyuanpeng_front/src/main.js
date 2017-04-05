0214295e31e0e1cd65059741a8531689bf519c70
xyp <xyp@kali.com>
2017-04-05 20:11:08 +0800

    starting on real things

--- xiyuanpeng_front/src/main.js
+++ xiyuanpeng_front/src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-// import router from './router'
+import router from './router'
 import iView from 'iview'
 import './assets/theme/index.less'
 
@@ -12,7 +12,7 @@ Vue.use(iView)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // router,
+  router,
   template: '<App/>',
   components: { App },
   render: h => h(App)
--- xiyuanpeng_front/src/router/index.js
+++ xiyuanpeng_front/src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '../components/Layout'
+import Blogs from '../components/Blogs'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Layout',
-      component: Layout
+      component: Layout,
+      children: [
+        {
+          path: 'blog',
+          name: 'blog',
+          component: Blogs
+        }
+      ]
     }
   ]
 })
