2bab957e47ae3ee04f96ccf1626710e16a4251e9
xyp <xyp@mint.com>
2017-07-08 11:58:17 +0800

    failed to merge 5

---
+++ backend/models/picture.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+type Picture struct {
+	Id          bson.ObjectId `json:"id" bson:"_id"  form:"id"`
+	Title       string        `json:"title" bson:"title"  form:"title"`
+	Path        string        `json:"path" bson:"path"  form:"path"`
+	Like        int           `json:"like" bson:"like"  form:"like"`
+	SlugUrl     string        `json:"slug_url" bson:"slug_url" form:"slug_url"`
+	Description string        `json:"description" bson:"description"  form:"description"`
+	CreatedAt   time.Time     `json:"created_at" bson:"created_at" form:"created_at"`
+	Comments    []Comment     `json:"comments" bson:"comments"  form:"comments"`
+}
---
+++ backend/utils/log_settings.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"go.uber.org/zap"
+	"os"
+)
+
+var (
+	DevCfg = zap.NewDevelopmentConfig()
+	ErrCfg = zap.NewDevelopmentConfig()
+	//ProdCfg = zap.NewProductionConfig()
+)
+
+func MyDevLogger() (*zap.Logger, error) {
+	DevCfg.OutputPaths = []string{
+		"stdout",
+		os.Getenv("MYGOPATH") + "/xieyuanpeng.in/logs/dev.log",
+	}
+	// this is the logging system error output paths
+	DevCfg.ErrorOutputPaths = []string{
+		"stderr",
+		os.Getenv("MYGOPATH") + "/xieyuanpeng.in/logs/sys_err.log",
+	}
+	return DevCfg.Build()
+}
+
+func MyErrLogger() (*zap.Logger, error) {
+	dyn := zap.NewAtomicLevel()
+	dyn.SetLevel(zap.ErrorLevel)
+	ErrCfg.Level = dyn
+	ErrCfg.OutputPaths = []string{
+		"stderr",
+		os.Getenv("MYGOPATH") + "/xieyuanpeng.in/logs/err.log",
+	}
+	// this is the logging system error output paths
+	ErrCfg.ErrorOutputPaths = []string{
+		"stderr",
+		os.Getenv("MYGOPATH") + "/xieyuanpeng.in/logs/sys_err.log",
+	}
+	return ErrCfg.Build()
+}
