889a20d387414a6fd4a3610f2f5c3e5b554d661e
pengye91 <pengye91@hotmail.com>
2017-07-30 17:50:24 +0800

    starting on styles in Uploads.vue

--- .xiyuanpeng_front/src/components/Uploads.vue
+++ .xiyuanpeng_front/src/components/Uploads.vue
@@ -104,7 +104,6 @@
           reader.readAsDataURL(file)
         }
         console.log(file)
-        console.log(this.uploadList)
         return false
       }
     },
@@ -114,9 +113,37 @@
   }
 </script>
 <style>
+.dropbox {
+    outline: 2px dashed grey; /* the dash box */
+    outline-offset: -10px;
+    background: lightcyan;
+    color: dimgray;
+    padding: 10px 10px;
+    min-height: 200px; /* minimum height */
+    position: relative;
+    cursor: pointer;
+  }
+
+  .input-file {
+    opacity: 0; /* invisible but it's there! */
+    width: 100%;
+    height: 200px;
+    position: absolute;
+    cursor: pointer;
+  }
+
+  .dropbox:hover {
+    background: lightblue; /* when mouse over to the drop zone, change color */
+  }
+
+  .dropbox p {
+    font-size: 1.2em;
+    text-align: center;
+    padding: 50px 0;
+  }
   .demo-upload-list {
     display: inline-block;
-    width: 60px;
+    width: 60%;
     height: 60px;
     text-align: center;
     line-height: 60px;
