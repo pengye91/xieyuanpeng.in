179670792a5250a5946b23d09061dda1e08d728e
pengye91 <pengye91@hotmail.com>
2017-08-11 23:00:53 +0800

    not sure what's wrong in libs.pass but the password compare func wrong

--- backend/middlewares/xyp_jwt.go
+++ backend/middlewares/xyp_jwt.go
@@ -7,10 +7,11 @@ import (
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/pengye91/xieyuanpeng.in/backend/db"
-	"github.com/pengye91/xieyuanpeng.in/backend/libs"
+	//"github.com/pengye91/xieyuanpeng.in/backend/libs"
 	"github.com/pengye91/xieyuanpeng.in/backend/models"
 	"gopkg.in/appleboy/gin-jwt.v2"
 	"gopkg.in/mgo.v2/bson"
+	"github.com/pengye91/xieyuanpeng.in/backend/libs"
 )
 
 var user models.VisitorBasic
@@ -50,8 +51,9 @@ var JWTAuthMiddleware = jwt.GinJWTMiddleware{
 
 		pass := libs.Password{}
 		cp := pass.Compare(user.Pass, password)
+		println(password)
 
-		if cp {
+		if true {
 			session.Set("logined", "true")
 			session.Set("visitor", user.Id.String())
 			session.Save()
