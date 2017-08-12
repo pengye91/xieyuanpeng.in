2e8fbb83f082ce47aed6e0bf30db9171e14755dc
pengye91 <pengye91@hotmail.com>
2017-08-12 16:47:15 +0800

    still works fine in sl410

--- .xiyuanpeng_front/src/components/User.vue
+++ .xiyuanpeng_front/src/components/User.vue
@@ -8,6 +8,7 @@
       @on-ok="asyncOK">
       <p>点击确定后，对话框将在 2秒 后关闭。</p>
     <logout></logout>
+    <Button v-if="user.name==='xyp'" shape="circle" @click="goToAdmin">Admin</Button>
     </Modal>
   </span>
 </template>
@@ -33,6 +34,10 @@
         setTimeout(() => {
           this.modal = false
         }, 2000)
+      },
+      goToAdmin () {
+        this.$router.push('/admin')
+        this.modal = false
       }
     },
     components: {
