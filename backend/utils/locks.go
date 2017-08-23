c3aaf8c093f1dfb8e5025071ea0950f7212f9a3f
xyp <xyp@mint.com>
2017-08-23 10:04:23 +0800

    added lock and timeout lock

--- backend/utils/locks.go
+++ backend/utils/locks.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -8,22 +9,49 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-func AcquireLock(lockname string, timeout int64) (string, error) {
+func AcquireLock(lockname string, acquireTimeout int64, lockTimeout int64) (string, error) {
 	conn := GlobalLockRedisPool.Get()
 	defer conn.Close()
 
 	identifier := string(uuid.NewV4())
 
-	endTime := time.Now().Unix() + timeout
+	endTime := time.Now().Unix() + acquireTimeout
 
 	for time.Now().Unix() < endTime {
-		if reply, err := redis.Bool(conn.Do("SETNX", "lock:"+lockname, identifier)); err != nil {
+		if reply, err := redis.Bool(conn.Do("SET", "lock:"+lockname, identifier, "EX", lockTimeout, "NX")); err != nil {
 			fmt.Println("setnx wrong:")
 			fmt.Println(err)
-			return identifier, err
+			time.Sleep(1 * time.Millisecond)
 		} else {
 			fmt.Println(reply)
 			return identifier, nil
 		}
 	}
+	return identifier, errors.New("TimeOut")
+}
+
+func ReleaseLock(lockname string, identifier string) bool {
+	conn := GlobalLockRedisPool.Get()
+	defer conn.Close()
+
+	lockname = "lock:" + lockname
+
+	for true {
+		conn.Send("WATCH", lockname)
+		if reply, _ := redis.String(conn.Do("GET", lockname)); reply == identifier {
+			conn.Send("MULTI")
+			conn.Send("DEL", lockname)
+			if reply, err := conn.Do("EXEC"); err != nil {
+				fmt.Println("EXEC err:")
+				fmt.Println(err)
+				continue
+			} else {
+				fmt.Println(reply)
+				return true
+			}
+
+		}
+		break
+	}
+	return false
 }
